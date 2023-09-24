3e1a586e09cbb4afa5887eae400e0dbc01dc347a
socketgut <71415224+varadpal@users.noreply.github.com>
2023-09-24 22:34:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function joined() {
+    let title = document.getElementById("result");
+    let btn = document.getElementById("btn");
+
+    let amount= document.getElementById("amnt").value;
+    let persons= document.getElementById("pers").value;
+    
+    let result= document.getElementById("result");
+
+    setTimeout(() => {
+        btn.style.backgroundColor = "grey"
+    }, 25);
+    setTimeout(() => {
+        btn.style.backgroundColor = "black"
+    }, 100);
+
+    if (amount > 0 && persons > 0) {
+        result.innerHTML= ("₹ "+ (amount/persons).toFixed(2) + " Per Person"); 
+    }else{
+        result.innerHTML= "Invalid Input"; 
+    }
+}
